e43e6fb73444c0b936ae4c6a8f71c3d7751370ef
Abdulrahman <azhryb824@gmail.com>
2026-05-16 14:14:41 +0300

    Fix frontend build and axios config

--- accounting_system/accounting_system_frontend/src/main.jsx
+++ accounting_system/accounting_system_frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './i18n' // تأكد من وجود ملف i18n.ts أو i18n.js في نفس المجلد
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
